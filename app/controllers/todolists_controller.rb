acc94c7a12065fd1f1bc79b2bcc6e701071bc41f
asakofujiki <a.fujiki1554@gmail.com>
2020-04-06 18:29:45 +0900

    [finish]3-7

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -1,7 +1,7 @@
 class TodolistsController < ApplicationController
   
  def new
-     @list =List.new
+     @list = List.new
  end
 
  def create
@@ -10,8 +10,27 @@ class TodolistsController < ApplicationController
 
 	 list.save
 
-	 redirect_to '/top'
+	 redirect_to todolist_path(list.id)
  end
+
+ def index
+ 	 @lists = List.all
+ end
+
+ def show
+ 	 @list = List.find(params[:id])
+ end
+
+ def edit
+ 	 @list = List.find(params[:id])
+ end
+
+ def update
+ 	 list = List.find(params[:id])
+ 	 list.update(list_params)
+ 	 redirect_to todolist_path(list.id)
+ end
+
  private
 
  def list_params
